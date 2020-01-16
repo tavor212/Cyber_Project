3d1ff8aa0578bcce1d9502079e812fd36ccaa1a4
tavor212 <tavor212@gmail.com>
2020-01-16 11:38:00 +0200

    updated file manager

--- file_manager_server.py
+++ file_manager_server.py
@@ -1,28 +1,25 @@
 import socket
 import threading
 import os
+import sys
 
+PORT = 6543
+HOST = '127.0.0.1'
+ALL_IP = '0.0.0.0'
 ERROR = 300
 RESPONSE = 200
 REQUEST = 100   # server asks the client for data
 CONFIRMATION = 10
 
-HOST = '127.0.0.1'
-PORT = 158
-
-# client sends request for a file(will look something like this):
-# 101test.txt
-# first part is the code part of the request: 101, second part is the data: test.txt
-
 
 def send_file(client_socket, file_location):
     if os.path.exists(file_location):
         client_socket.send(CONFIRMATION + os.path.getsize(file_location))
-        user_answer = client_socket.recv(1024)
+        user_answer = client_socket.recv(5120)
         if user_answer[:2] == CONFIRMATION:
-            """reads the file 1024 bytes at a time."""
+            """reads the file 5120 bytes at a time."""
             with open(file_location, 'rb') as f:
-                file_parts = f.read(1024)
+                file_parts = f.read(5120)
                 client_socket.send(file_parts)
                 """if the file is bigger than 1024 bytes then the func reads another 1024 bytes and checks if the file is empty"""
                 while os.path.getsize(file_location) != "":
@@ -59,15 +56,59 @@ def delete_file(client_socket, file_location):
     client_socket.close()
 
 
+def handel_thread(connection, ip, port, max_buffer_size=5120):
+    while True:
+        client_input = receive_input(connection, max_buffer_size)
+
+
+def receive_input(connection, max_buffer_size):
+    client_input = connection.recv(max_buffer_size)
+    client_input_size = sys.getsizeof(client_input)
+
+    if client_input_size == max_buffer_size:
+        print("The input size is greater than expected, let me divide them")
+        temp = "1"
+        while temp != "":
+            temp = connection.recv(max_buffer_size)
+            client_input += temp
+    decoded_input = client_input.decode("utf8").rstrip()  # decode and strip end of line
+    result = process_input(decoded_input)
+
+    return result
+
 
 def main():
-    s = socket.socket()
-    s.bind((HOST, PORT))
+    print("hello")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    """tells the operating system to reuse the port if something crashes or a user is disconnecting"""
+    print("Socket created")
+
+    try:
+        s.bind((ALL_IP, PORT))
+
+    except:
+        print("ooh something went wrong")
+        sys.exit()
+
     s.listen(5)
     print("server started")
+    # infinite loop- do not reset for every requests
+    while True:
+        connection, address = s.accept()
+        ip, port = str(address[0]), str(address[1])
+        print("Connected with " + ip + " :" + port)
+
+        try:
+            """sends the threads to a func that checks their requests and redirects them"""
+            Thread(target=handle_thread, args=(connection, ip, port)).start()
+        except:
+            print("Thread did not start.")
+
+    soc.close()
 
 
-if __name__ == "main":
+if __name__ == '__main__':
     main()
         
 
