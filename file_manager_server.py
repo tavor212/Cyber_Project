4315d91b2961b5473b1407947ee8635dd08820c3
tavor212 <tavor212@gmail.com>
2020-01-27 14:17:17 +0200

    need to check if this is better than previous commit

--- file_manager_server.py
+++ file_manager_server.py
@@ -3,31 +3,32 @@ import threading
 import os
 import sys
 
+
 PORT = 6543
 HOST = '127.0.0.1'
 ALL_IP = '0.0.0.0'
-ERROR = 300
-RESPONSE = 200
-REQUEST = 100   # server asks the client for data
-SEND = 40
-CHANGE_NAME = 30
-DELETE = 20
-EXIT = 10
-CONFIRMATION = 1
+ERROR = "300".encode()
+RESPONSE = "200".encode()
+REQUEST = "100".encode()
+SEND_FILE = "40".encode()
+CHANGE_NAME = "30".encode()
+DELETE = "20".encode()
+EXIT = "10".encode()
+CONFIRMATION = "1".encode()
 
 
 def send_file(client_socket, file_location):
     if os.path.exists(file_location):
-        client_socket.send(CONFIRMATION + os.path.getsize(file_location))
+        client_socket.send(CONFIRMATION, str(os.path.getsize(file_location)).encode())
         user_answer = client_socket.recv(5120)
         if user_answer[:2] == CONFIRMATION:
             """reads the file 5120 bytes at a time."""
             with open(file_location, 'rb') as f:
                 file_parts = f.read(5120)
                 client_socket.send(file_parts)
-                """if the file is bigger than 1024 bytes then the func reads another 1024 bytes and checks if the file is empty"""
+                """if the file is bigger than 5120 bytes then the func reads another 1024 bytes and checks if the file is empty"""
                 while os.path.getsize(file_location) != "":
-                    file_parts = f.read(1024)
+                    file_parts = f.read(5120)
                     client_socket.send(file_parts)
         else:
             client_socket.send(ERROR)
@@ -39,13 +40,13 @@ def send_file(client_socket, file_location):
 
 def change_file_name(client_socket, file_location, new_file_name):
     if os.path.exists(file_location):
-        client_socket.send(CONFIRMATION)
         """gets the dir of the file with dirname"""
         dir_path = os.path.dirname(file_location)
         """creates a new path with the directory, back slash and the new file name"""
         new_path = dir_path + "\\" + new_file_name
         """the func rename basiclly copies the original just with a diffrent name"""
         os.rename(file_location, new_path)
+        client_socket.send(CONFIRMATION)
     else:
         client_socket.send(ERROR)
     client_socket.close()
@@ -53,8 +54,8 @@ def change_file_name(client_socket, file_location, new_file_name):
 
 def delete_file(client_socket, file_location):
     if os.path.exists(file_location):
-        client_socket.send(CONFIRMATION)
         os.remove(file_location)
+        client_socket.send(CONFIRMATION)
     else:
         client_socket.send(ERROR)
     client_socket.close()
@@ -62,15 +63,15 @@ def delete_file(client_socket, file_location):
 
 def handel_thread(connection, ip, port, max_buffer_size=5120):
     active = True
+    client_input = receive_input(connection, max_buffer_size)
     while active:
-        client_input = receive_input(connection, max_buffer_size)
         if client_input[3:] == EXIT:
             print("Client is requesting to quit")
             connection.close()
             message = "Connection " + ip + ": " + port + " closed"
             print(message)
             is_active = False
-        if client_input[3:] == SEND:
+        if client_input[3:] == SEND_FILE:
             file_location = client_input[3:]
             send_file(connection, file_location)
         if client_input[3:] == DELETE:
@@ -84,7 +85,7 @@ def handel_thread(connection, ip, port, max_buffer_size=5120):
             new_file_name = client_input[1]
             change_file_name(connection, file_location, new_file_name)
         else:
-            connection.send("oh oh something went wrong")
+            connection.send("oh oh something went wrong".encode())
             print("there is a problem with the input")
             connection.close()
             active = False
@@ -100,8 +101,8 @@ def receive_input(connection, max_buffer_size):
         while temp != "":
             temp = connection.recv(max_buffer_size)
             client_input += temp
-    decoded_input = client_input.decode("utf8").rstrip()  # decode and strip end of line
-    result = str(decoded_input).upper()
+    decoded_input = client_input.decode().rstrip()  # decode and strip end of line
+    result = str(decoded_input)
 
     return result
 
@@ -109,18 +110,18 @@ def receive_input(connection, max_buffer_size):
 def main():
     print("hello")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     """tells the operating system to reuse the port if something crashes or a user is disconnecting"""
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print("Socket created")
 
     try:
         s.bind((ALL_IP, PORT))
 
     except:
-        print("ooh something went wrong")
+        print("oh oh something went wrong")
         sys.exit()
 
-    s.listen(5)
+    s.listen(7)
     print("server started")
     # infinite loop- do not reset for every requests
     while True:
@@ -133,8 +134,9 @@ def main():
             threading.Thread(target=handel_thread, args=(connection, ip, port)).start()
         except:
             print("Thread did not start.")
-
-    soc.close()
+            connection.send(ERROR)
+            connection.close()
+    s.close()
 
 
 if __name__ == '__main__':
