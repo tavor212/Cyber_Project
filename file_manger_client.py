f9d402dfebc3a0c95a9dd6df4deb886ca6f67338
tavor212 <tavor212@gmail.com>
2020-01-30 11:18:11 +0200

    need to change change name so it will work on the same file twice

--- file_manger_client.py
+++ file_manger_client.py
@@ -19,6 +19,7 @@ def main():
     print("What would you like me to do?\n")
 
     active = True
+    file_format = ""
     while active:
         print("'Send File' - a function that with a given path will send you a file from the server to your computer")
         print("'Change Name' - a function that with a given path the a file, will change its name with a name you give")
@@ -27,10 +28,29 @@ def main():
         message = input()
         if message.upper() == "EXIT":
             active = False
-            print("ok bye :(")
             s.send(DICT['EXIT'])
+            message = s.recv(1024)
+            if message == DICT['EXIT']:
+                print("Ok bye :(")
+                sys.exit()
         elif message.upper() == "SEND FILE":
-            pass
+            print("What is the file path")
+            path = input()
+            message = DICT['SEND_FILE'] + path.encode()
+            s.send(message)
+            message = s.recv(1024)
+            if message[:1] == DICT['CONFIRMATION']:
+                message = message[1:].decode()
+                print(message + '\n' + "This is the file size. Do you still want to download?")
+                answer = input()
+                if answer.upper() == "YES":
+                    s.send(DICT['CONFIRMATION'])
+                elif answer.upper() == "NO":
+                    print("Ok. Cancelling...")
+                else:
+                    print("Sorry, this is not a valid request")
+            else:
+                print("Oh oh something went wrong.\n")
         elif message.upper() == "DELETE":
             print("What is the file path?")
             message = DICT['DELETE']+input().encode()
@@ -39,11 +59,28 @@ def main():
             if message == DICT['CONFIRMATION']:
                 print("The file was deleted!!\n")
             if message == DICT['ERROR']:
-                print("oh oh something went wrong\n")
+                print("Oh oh. Something went wrong. The file was not deleted")
         elif message.upper() == "CHANGE NAME":
-            pass
+            print("What is the path of the file you want to change the name of")
+            path = input()
+            for char in path[::-1]:
+                if char != '.':
+                    file_format += char
+                else:
+                    break
+            file_format = file_format[::-1]
+            print("What is the new file name?")
+            new_name = input()
+            new_name = new_name + "." + file_format
+            message = DICT['CHANGE_NAME'] + path.encode() + ' '.encode() + new_name.encode()
+            s.send(message)
+            message = s.recv(1024)
+            if message == DICT['CONFIRMATION']:
+                print("The name was succsfuly changed\n")
+            if message == DICT['ERROR']:
+                print("Oh oh something went wrong. The file was not changed\n")
         else:
-            print("there is a problem with your input, please try again")
+            print("there is a problem with your input, please try again\n")
 
 
 if __name__ == "__main__":
